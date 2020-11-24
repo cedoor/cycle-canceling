00b38c2b8303f442608afb0aa15a303a4bfc3de7
cedoor <me@cedoor.dev>
2020-11-24 17:35:25 +0100

    [docs] update cycle-canceling function comments

--- src/minimumCostFlowAlgorithms.ts
+++ src/minimumCostFlowAlgorithms.ts
@@ -4,11 +4,12 @@ import { bellmanFord } from "./shortestPathAlgorithms"
 import { getResidualCapacity, sendFlow, setResidualGraph } from "./utils"
 
 /**
- * The Cycle-Canceling algorithm is a minimum-cost flow problem
- * and starting from a feasible graph obtained with a maximum flow algorithm
- * and as long as negative cost cycles exist augment the flow along this cycles.
- * This implementation uses the Edmonds-Karp algorithm to solve the maximum flow problem,
- * and the Bellman-Fort algorithm to find the negative cycles.
+ * The Cycle-Canceling algorithm solves the minimum-cost flow problem
+ * starting from a feasible graph obtained with a maximum flow algorithm
+ * and, as long as negative cost cycles exist, augment the flow along
+ * this cycles. This implementation uses the Edmonds-Karp algorithm to
+ * solve the maximum flow problem in O(n * m^2), and the Bellman-Fort
+ * algorithm to find the negative cycles in O(m * n).
  * Time complexity: O (n * m^2 * C * U).
  * @param {Graph} The graph to visit.
  * @returns {[number, number]} The maximum flow and the minimum cost.
